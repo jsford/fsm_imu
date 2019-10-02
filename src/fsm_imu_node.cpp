5d771955491b6f5a131756ddaa94956d12c772fa
Jordan Ford <jsford94@gmail.com>
2019-10-02 16:44:55 -0400

    Publish IMU data to ROS topic. Not sure about transforms and units yet.

--- src/fsm_imu_node.cpp
+++ src/fsm_imu_node.cpp
@@ -2,6 +2,12 @@
 #include <freespace/freespace_printers.h>
 #include <freespace/freespace_util.h>
 #include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/MagneticField.h>
+#include <stdint.h>
+#include <string>
+
+const double earth_gravity_g = 9.81998; // m/s^2
 
 // The names corresponding to the sensor indices
 const char* const SENSOR_NAMES[] = {
@@ -14,15 +20,6 @@ const char* const SENSOR_NAMES[] = {
     "Sensor Fusion"
 };
 
-/**
- * sendGetSensorPeriodMessage
- * Sends a message to read the sample rate
- * of a sensor on a Freespace device.
- * device - The Freespace Device ID of the device
- * sensor - The sensor index - see the HCOMM document
- * for more information
- * return - The return code of the message
- */
 int sendGetSensorPeriodMessage(FreespaceDeviceId device, int sensor) {
 	struct freespace_message message;
 
@@ -32,6 +29,21 @@ int sendGetSensorPeriodMessage(FreespaceDeviceId device, int sensor) {
 	message.messageType = FREESPACE_MESSAGE_SENSORPERIODREQUEST;
 	message.sensorPeriodRequest.get = 1;  // We are getting, not setting
 	message.sensorPeriodRequest.sensor = sensor; // Sensor index - see HCOMM doc for more info
+
+	return freespace_sendMessage(device, &message);
+}
+
+int sendSetSensorPeriodMessage(FreespaceDeviceId device, int sensor, uint32_t period_us) {
+	struct freespace_message message;
+
+	// Send the sensor period message with the user parameters
+	memset(&message, 0, sizeof(message)); // Make sure all the message fields are initialized to 0.
+
+	message.messageType = FREESPACE_MESSAGE_SENSORPERIODREQUEST;
+	message.sensorPeriodRequest.get = 0;  // We are getting, not setting
+	message.sensorPeriodRequest.sensor = sensor; // Sensor index - see HCOMM doc for more info
+    message.sensorPeriodRequest.period = period_us;
+    message.sensorPeriodRequest.commit = 1;
 
 	return freespace_sendMessage(device, &message);
 }
@@ -59,43 +71,38 @@ int waitForPeriodResponse(FreespaceDeviceId device, int* sensorValue, int* perio
 }
 
 
-int printSensorInfo(FreespaceDeviceId device) {
+int setSensorPeriod(FreespaceDeviceId device, uint32_t imu_period) {
 	int rc;
 	int index;
 	int sensor;
 	int period;
 
 	// Update sensor information
-	printf("Sensors:\n");
-	for (index = 0;index < 7;index++) {
+	for (index = 0; index < 7;index++) {
 		// Request the sensor period information
-		rc = sendGetSensorPeriodMessage(device, index);
+		rc = sendSetSensorPeriodMessage(device, index, imu_period);
 		if (rc != FREESPACE_SUCCESS) {
 			return rc;
 		}
 
 		// Wait for a response
 		rc = waitForPeriodResponse(device, &sensor, &period);
-
-		if (rc == FREESPACE_ERROR_TIMEOUT) { // Indicates timeout
-			printf("     %d. %s TIMED OUT.\n", index, SENSOR_NAMES[index]);
-		} else if (rc == FREESPACE_SUCCESS) {
-			printf("     %d. %s", sensor, SENSOR_NAMES[index]);
-			if (period != 0)
-				printf(" @ %d us.\n", period);
-			else
-				printf(" disabled.\n");
-		} else {
-			return rc;
-		}
-	}
-	return FREESPACE_SUCCESS;
-	printf("\n");
+    }
+    return rc;
 }
 
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "fsm_imu");
+    ros::NodeHandle nh("~");
+
+    int imu_period;
+    std::string imu_frame_id, topic;
+    nh.param<int>("period_us", imu_period, 8000);
+    nh.param<std::string>("frame_id", imu_frame_id, "/imu");
+    nh.param<std::string>("topic", topic, "/imu/data");
+
+    ros::Publisher pubImu = nh.advertise<sensor_msgs::Imu>(topic, 100);
 
     // Initialize the freespace library
     int rc = freespace_init();
@@ -114,7 +121,6 @@ int main(int argc, char** argv) {
     }
 
     // Prepare to communicate with the device found above
-    printf("Found a device. Trying to open it...\n");
     rc = freespace_openDevice(device);
     if (rc != FREESPACE_SUCCESS) {
         printf("Error opening device: %d\n", rc);
@@ -144,16 +150,14 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    // Print out the sensor info
-	rc = printSensorInfo(device);
-	if (rc != FREESPACE_SUCCESS) {
-		printf("Error getting sensor info: %d\n", rc);
-		return 1;
-	}
+    rc = setSensorPeriod(device, imu_period);
+    if (rc != FREESPACE_SUCCESS) {
+        printf("Error setting device period.\n");
+        return 1;
+    }
 
-     // Configure the device for motion outputs
+    // Configure the device for motion outputs
     struct freespace_message message;
-    printf("Sending message to enable motion data.\n");
     memset(&message, 0, sizeof(message)); // Make sure all the message fields are initialized to 0.
 
     message.messageType = FREESPACE_MESSAGE_DATAMODECONTROLV2REQUEST;
@@ -174,6 +178,8 @@ int main(int argc, char** argv) {
         printf("Could not send message: %d.\n", rc);
     }
 
+    unsigned int imu_seq = 0;
+
     // Loop to read messages
     while (ros::ok()) {
         rc = freespace_readMessage(device, &message, 100);
@@ -192,49 +198,47 @@ int main(int argc, char** argv) {
         }
 
         if (message.messageType == FREESPACE_MESSAGE_MOTIONENGINEOUTPUT) {
-            struct MultiAxisSensor accel, accelNoGrav, actClass, angPos, angVel,
-                                   compass, inclination, mag, temp;
+            struct MultiAxisSensor accel, accelNoGrav, actClass, angPos,
+                                   angVel, compass, inclination, mag;
 
-            // Extract Acceleration
-            rc = freespace_util_getAcceleration(&message.motionEngineOutput, &accel);
-            if (rc == 0) {
-                printf ("Accel: X: % 6.2f, Y: % 6.2f, Z: % 6.2f\n", accel.x, accel.y, accel.z);
-            }
-            // Extract AccelerationNoGravity
-            rc = freespace_util_getAcceleration(&message.motionEngineOutput, &accelNoGrav);
-            if (rc == 0) {
-                printf ("AccelNoGrav: X: % 6.2f, Y: % 6.2f, Z: % 6.2f\n", accelNoGrav.x, accelNoGrav.y, accelNoGrav.z);
-            }
-            // Extract ActionClass
-            rc = freespace_util_getActClass(&message.motionEngineOutput, &actClass);
-            if (rc == 0) {
-                printf ("ActionClassFlags: % 6.2f, PowerMgmtFlags: % 6.2f\n", actClass.x, actClass.y);
-                // Note(Jordan): actClass.x == 1 (STABLE), actClass.x == 2 (MOSTLY STABLE), actClass.y == 4 (MOVING)
-            }
-            // Extract AngularVelocity
-            rc = freespace_util_getAngularVelocity(&message.motionEngineOutput, &angVel);
-            if (rc == 0) {
-                printf ("AngVel: X: % 6.2f, Y: % 6.2f, Z: % 6.2f\n", angVel.x, angVel.y, angVel.z);
-            }
-            // Extract AngularPosition
-            rc = freespace_util_getAngPos(&message.motionEngineOutput, &angPos);
-            if (rc == 0) {
-                printf ("AngPos: X: % 6.2f, Y: % 6.2f, Z: % 6.2f\n", angPos.x, angPos.y, angPos.z);
-            }
-            // Extract Compass
-            rc = freespace_util_getCompassHeading(&message.motionEngineOutput, &compass);
-            if (rc == 0) {
-                printf ("Compass: % 6.2f\n", compass.x);
-            }
-            // Extract Inclination
-            rc = freespace_util_getInclination(&message.motionEngineOutput, &inclination);
-            if (rc == 0) {
-                printf ("Inclination: % 6.2f\n", inclination.x);
-            }
-            // Extract Magnetometer
-            rc = freespace_util_getMagnetometer(&message.motionEngineOutput, &mag);
-            if (rc == 0) {
-                printf ("Mag: X: % 6.2f, Y: % 6.2f, Z: % 6.2f\n", mag.x, mag.y, mag.z);
+            bool have_accel       = (0 == freespace_util_getAcceleration(&message.motionEngineOutput, &accel));
+            bool have_accelNoGrav = (0 == freespace_util_getAcceleration(&message.motionEngineOutput, &accelNoGrav));
+            bool have_ActClass    = (0 == freespace_util_getActClass(&message.motionEngineOutput, &actClass));
+            bool have_angVel      = (0 == freespace_util_getAngularVelocity(&message.motionEngineOutput, &angVel));
+            bool have_angPos      = (0 == freespace_util_getAngPos(&message.motionEngineOutput, &angPos));
+            bool have_compass     = (0 == freespace_util_getCompassHeading(&message.motionEngineOutput, &compass));
+            bool have_inclination = (0 == freespace_util_getInclination(&message.motionEngineOutput, &inclination));
+            bool have_mag         = (0 == freespace_util_getMagnetometer(&message.motionEngineOutput, &mag));
+
+            if(have_accel && have_angVel && have_angPos) {
+                sensor_msgs::Imu msg;
+                msg.header.seq = imu_seq++;
+                msg.header.stamp = ros::Time::now();
+                msg.header.frame_id = imu_frame_id;
+
+                // Are these units correct? I think so.
+                msg.orientation.x = angPos.x;
+                msg.orientation.y = angPos.y;
+                msg.orientation.z = angPos.z;
+                msg.orientation.w = angPos.w;
+                std::fill(std::begin(msg.orientation_covariance),
+                          std::end(msg.orientation_covariance), 0.0);
+
+                // Are these units correct? Not sure.
+                msg.angular_velocity.x = angVel.x;
+                msg.angular_velocity.y = angVel.y;
+                msg.angular_velocity.z = angVel.z;
+                std::fill(std::begin(msg.angular_velocity_covariance),
+                          std::end(msg.angular_velocity_covariance), 0.0);
+
+                // Are these units correct? Not sure.
+                msg.linear_acceleration.x = accel.x * earth_gravity_g;
+                msg.linear_acceleration.y = accel.y * earth_gravity_g;
+                msg.linear_acceleration.z = accel.z * earth_gravity_g;
+                std::fill(std::begin(msg.linear_acceleration_covariance),
+                          std::end(msg.linear_acceleration_covariance), 0.0);
+
+                pubImu.publish(msg);
             }
         }
     }
